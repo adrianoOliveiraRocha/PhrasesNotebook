64c22ff12a65ac96855cfaa1e841d46a27df5288
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2020-11-05 14:00:57 -0300

    #

--- screens/GetPhrase.js
+++ screens/GetPhrase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, ToastAndroid } from 'react-native';
 import styles from './styles/main';
 import Phrase from './../models/Phrase';
 import * as Speech from 'expo-speech';
@@ -7,13 +7,32 @@ import * as Speech from 'expo-speech';
 function GetPhrase({ navigation }) {
   const [phrase, setPhrase] = React.useState(null);
   const [id, setId] = React.useState(null);
+  const [phraseExists, setPhraseExists] = React.useState(true);
 
   function speak() {
-    Speech.speak(phrase);
+
+    const start = () => {
+      ToastAndroid.show('Falando...', ToastAndroid.SHORT);
+    }
+
+    var options = {
+      language: 'en-AU',
+      pitch: 1.0,
+      rate: 1.0,
+      onStart: start,
+      onError: (error) => alert('Oops! Tivemos um erro: ' + error),
+    };
+
+    Speech.speak(
+      phrase,
+      options,
+    );
+
   }
 
   function choosePhrase(phrases) {
     var choosedPhrase = null;
+
     for (var i in phrases) {
       if(!phrases[i].used) {
         choosedPhrase = phrases[i].text;
@@ -21,9 +40,17 @@ function GetPhrase({ navigation }) {
         break;
       }
     }
+
     if(choosedPhrase) setPhrase(choosedPhrase);
-    else setPhrase("Você já usou todas");
+    else {
+      setPhrase("Você já usou todas as frases registradas. Pressione Home para "
+      +"voltar à tela inicial e pressione o botão reiniciar");
+      setPhraseExists(false);
+    }
     makeAsUsed();
+
+
+
   }
 
   function makeAsUsed() {
@@ -59,7 +86,17 @@ function GetPhrase({ navigation }) {
       })
   })();
 
-  if(id) {
+  function nextOne() {
+    Phrase.getPhrase()
+      .then(phrases => {
+        choosePhrase(phrases);
+      })
+      .catch(e => {
+        alert(e);
+      })
+  }
+
+  if(id && phraseExists) {
     return (
       <View style={styles.formContainer}>
         <View style={styles.formField}>
@@ -68,14 +105,19 @@ function GetPhrase({ navigation }) {
 
         <View style={[styles.buttonsContainer, {height: 100}]}>
           <Button
-            title="Deletar"
-            color='#aa3344'
-            onPress={deletePhrase}
+            title="Próxima"
+            color='#089757'
+            onPress={nextOne}
           />
           <Button
             title="Ouvir"
             color='#089757'
             onPress={speak}
+          />
+          <Button
+            title="Deletar"
+            color='#aa3344'
+            onPress={deletePhrase}
           />
         </View>
 
@@ -85,12 +127,19 @@ function GetPhrase({ navigation }) {
     return (
       <View style={styles.formContainer}>
         <View style={styles.formField}>
-          <Text style={styles.formLabel}>{phrase}</Text>
+          <Text style={[styles.formLabel]}>{phrase}</Text>
+        </View>
+        <View style={[styles.buttonsContainer, {height: 100}]}>
+          <Button
+            title="Home"
+            color='#089757'
+            onPress={() => navigation.navigate("Home")}
+          />
+
         </View>
       </View>
     );
   }
-
 }
 
 export default GetPhrase;
--- screens/styles/main.js
+++ screens/styles/main.js
@@ -48,6 +48,7 @@ const styles = StyleSheet.create({
     color: 'white',
     marginHorizontal: 5,
     fontSize: 20,
+    textAlign: 'center'
   },
 
   formItemInput: {
